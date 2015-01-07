ea9b1c51aaba28c1bdd06202ea8c4b5520e61130
root <root@dgaschhome.com.ar>
2015-01-07 15:03:03 -0300

    First update

---
+++ app/cache/dev/twig/83/fe/4f7f6ec71bc5da0e9a286199638e8cc972994b2cb4e1506cd37227f46106.php
@@ -0,0 +1,49 @@
+<?php
+
+/* default/index.html.twig */
+class __TwigTemplate_83fe4f7f6ec71bc5da0e9a286199638e8cc972994b2cb4e1506cd37227f46106 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = $this->env->loadTemplate("base.html.twig");
+
+        $this->blocks = array(
+            'body' => array($this, 'block_body'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_body($context, array $blocks = array())
+    {
+        // line 4
+        echo "    Homepage.
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "default/index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  31 => 4,  28 => 3,);
+    }
+}
